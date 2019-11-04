4c1c2af60a0678fe31318f955f2d08b18dd601a3
rjebbari <jebbarirayhane@gmail.com>
2019-11-04 22:17:43 +0100

    login

---
+++ Tracker De Bug/BootStrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Tracker_De_Bug.ViewModels;
+
+namespace Tracker_De_Bug
+{
+    class BootStrapper:BootstrapperBase 
+    {
+        public BootStrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
